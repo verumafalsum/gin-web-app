57adadbcb48ca46c5be6c37c380a8ae311086e78
Vladimir Rudakov <vladimirrudd@gmail.com>
2020-10-17 19:10:10 +0300

    init commit

---
+++ handlers.article_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Test that a GET request to the home page returns the home page with
+// the HTTP code 200 for an unauthenticated user
+func TestShowIndexPageUnauthenticated(t *testing.T) {
+	r := getRouter(true)
+
+	r.GET("/", showIndexPage)
+
+	// Create a request to send to the above route
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		// Test that the http status code is 200
+		statusOK := w.Code == http.StatusOK
+
+		// Test that the page title is "Home Page"
+		p, err := ioutil.ReadAll(w.Body)
+		pageOK := err == nil && strings.Index(string(p), "<title>Home Page</title>") > 0
+
+		return statusOK && pageOK
+	})
+}
+
+func TestArticleListJSON(t *testing.T) {
+	r := getRouter(true)
+
+	r.GET("/", showIndexPage)
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("Accept", "application/json")
+
+	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		statusOK := w.Code == http.StatusOK
+
+		r, err := ioutil.ReadAll(w.Body)
+		var js json.RawMessage
+		responseOK := err == nil && json.Unmarshal([]byte(r), &js) == nil
+
+		return statusOK && responseOK
+	})
+}
